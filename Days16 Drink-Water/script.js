4c98fcb9b6383dc06b37baad9a112cea7d5f1eb5
Bowu Chen <1028505817@qq.com>
2021-10-10 21:19:08 +0800

    ':art:'

--- Days16 Drink-Water/script.js
+++ Days16 Drink-Water/script.js
@@ -9,5 +9,42 @@ smallCups.forEach((cup, idx) => {
 });
 
 function highlightCups(idx) {
-    console.log(idx);
+    if (smallCups[idx].classList.contains('full') &&
+        (idx === smallCups.length - 1 ||
+            !smallCups[idx].nextElementSibling.classList.contains('full'))) {
+        idx--;
+    }
+    smallCups.forEach((cup, i) => {
+        if (i <= idx) {
+            cup.classList.add('full');
+        } else {
+            cup.classList.remove('full');
+        }
+    });
+
+    updateBigCup();
+}
+
+function updateBigCup() {
+    console.time('Test');
+    const fullCups = document.querySelectorAll('.cup-small.full').length;
+    const totalCups = smallCups.length;
+
+    if (fullCups === 0) {
+        percentage.style.visibility = 'hidden';
+        percentage.style.height = 0;
+    } else {
+        percentage.style.visibility = 'visible';
+        percentage.style.height = `${fullCups / totalCups * 330}px`;
+        percentage.innerText = `${fullCups / totalCups * 100}%`;
+    }
+
+    if (fullCups === totalCups) {
+        remained.style.visibility = 'hidden';
+        remained.style.height = 0;
+    } else {
+        remained.style.visibility = 'visible';
+        liters.innerText = `${2 - 250 * fullCups / 1000}L`;
+    }
+    console.timeEnd('Test');
 }
