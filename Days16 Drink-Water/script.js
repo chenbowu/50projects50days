7f015eaf1998d0becf2d1c8965d6f137f4d5431d
Bowu Chen <1028505817@qq.com>
2021-10-11 15:54:01 +0800

    ':ambulance:'

--- Days16 Drink-Water/script.js
+++ Days16 Drink-Water/script.js
@@ -11,8 +11,8 @@ smallCups.forEach((cup, idx) => {
 function highlightCups(idx) {
     if (smallCups[idx].classList.contains('full') &&
         !smallCups[idx].nextElementSibling?.classList.contains('full')) {
-            idx--;
-        }
+        idx--;
+    }
     smallCups.forEach((cup, i) => {
         if (i <= idx) {
             cup.classList.add('full');
