822f791ef85e45b0c3a6df0996ee9d7aab059f4d
Bowu Chen <1028505817@qq.com>
2021-10-12 22:58:20 +0800

    :art:

--- Days18 background-slider/script.js
+++ Days18 background-slider/script.js
@@ -1,36 +1,35 @@
 const body = document.body;
 const slides = document.querySelectorAll('.slide');
-const btnNext = document.getElementById('right');
 const btnPrev = document.getElementById('left');
+const btnNext = document.getElementById('right');
 let currentActive = 0;
-
-btnNext.addEventListener('click', () => {
-    currentActive++;
-    if (currentActive > slides.length - 1) {
-        currentActive = 0;
-    }
-    setBgToBody();
-    setActiveSlide();
-});
 
 btnPrev.addEventListener('click', () => {
     currentActive--;
     if (currentActive < 0) {
         currentActive = slides.length - 1;
     }
+    setBgToSlide();
     setBgToBody();
-    setActiveSlide();
 });
+
+btnNext.addEventListener('click', () => {
+    currentActive++;
+    if (currentActive > slides.length - 1) {
+        currentActive = 0;
+    }
+    setBgToSlide();
+    setBgToBody();
+})
 
 setBgToBody();
 
-function setBgToBody() {
-    body.style.backgroundImage = slides[currentActive].style.backgroundImage;
-}
-
-
-function setActiveSlide() {
-    [].find.call(slides, slide => slide.classList.contains('active'))
+function setBgToSlide() {
+    [].find.call(slides,slide => slide.classList.contains('active'))
         .classList.remove('active');
     slides[currentActive].classList.add('active');
+}
+
+function setBgToBody() {
+    body.style.backgroundImage = slides[currentActive].style.backgroundImage;
 }
