d2ed0c68e7e78ba6afb2e45c183c37381511bb0e
Bowu Chen <1028505817@qq.com>
2021-10-18 15:59:10 +0800

    :fire:

--- Days19 theme-clock/script.js
+++ Days19 theme-clock/script.js
@@ -7,7 +7,6 @@ const toggle = document.querySelector('.toggle');
 
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
 toggle.addEventListener('click', (e) => {
     const html = document.querySelector('html');
     if (html.classList.contains('dark')) {
